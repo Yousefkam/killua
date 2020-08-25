dc21ff8c61767a67a9fa31949591f402aa7271fa
Yousef Kammouneh <61968167+Yousefkam@users.noreply.github.com>
2020-08-25 13:40:19 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+function relocate_home(){
+    location.href = "https://discord.com/api/oauth2/authorize?client_id=720193155730767934&permissions=8&scope=bot"
+  }
+function relocate_server(){
+    location.href = 'https://discord.gg/qyvpwtH'
+}
